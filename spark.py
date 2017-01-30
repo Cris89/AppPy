f7fc90e314a57816f18d119057adcf7f34da3e40
Cris <cris.dimarco.1897@gmail.com>
2017-01-30 12:23:45 +0100

    Update

--- spark.py
+++ spark.py
@@ -1,30 +1,30 @@
 '''
 Created on Oct 30, 2016
-
+ 
 @author: cris
 '''
-
+ 
 from pyspark import SparkContext, SparkConf
-
+ 
 DATA = "data.txt"
 COUNT = "count.txt"
-
+ 
 class Spark():
     '''
     classdocs
     '''
-
+ 
     def __init__(self):
         '''
         Constructor
         '''
-    
+     
     logFile = DATA
     conf = SparkConf().setAppName("Count").setMaster("local")
     sc = SparkContext(conf = conf)
     logData = sc.textFile(logFile).cache()
     num = str(logData.count())
-    
+     
     count = open(COUNT, "a")
     count.write(num)
     count.write("\n")
